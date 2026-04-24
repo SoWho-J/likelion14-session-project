03d0efaaac9851255b24deb722512e0b73aff528
sowho.j <jangseohu112@gmail.com>
2026-04-24 15:23:53 +0900

    ✨ Feat:4주차 세션  작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RootLayout from "./src/layout/RootLayout.jsx";
+import RootLayout from "./layout/RootLayout.jsx";
 import Main from "./pages/Main/Main.jsx";
 
 function App() {
@@ -8,6 +8,8 @@ function App() {
       <Routes>
         <Route element={<RootLayout />}>
           <Route path="/" element={<Main />} />
+          <Route path="/add" element={<></>} />{" "}
+          {/* 추후 element에 상품 등록 페이지 들어가야함 */}
         </Route>
       </Routes>
     </BrowserRouter>
