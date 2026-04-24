4d7876ef57ea6e1aaaf44c4377764a75576970ca
sowho.j <jangseohu112@gmail.com>
2026-04-24 19:40:56 +0900

    ✨Feat:필터 버튼 만들기

---
+++ src/components/common/button/FilterButton.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import ArrowIcon from "../../../assets/icons/Vector.svg";
+
+export default function FilterButton({ text, onClick }) {
+  return (
+    <StyledFilterButton onClick={onClick}>
+      <span>{text}</span>
+      <Arrow src={ArrowIcon} alt="arrow" />
+    </StyledFilterButton>
+  );
+}
+
+const StyledFilterButton = styled.button`
+  display: flex;
+  min-width: 58px;
+  padding: 8px 10px 11px 10px;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+
+  border: none;
+  border-radius: 20px;
+  background: #f2f2f2;
+
+  color: #555;
+  font-size: 13px;
+  cursor: pointer;
+
+  white-space: nowrap;
+`;
+const Arrow = styled.img`
+  width: 10px;
+  height: 5px;
+`;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,26 @@
+import styled from "styled-components";
+import FilterButton from "../../components/common/button/FilterButton";
+
 export default function Main() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <ButtonWrap>
+      <FilterButton text="성별" />
+      <FilterButton text="색상" />
+      <FilterButton text="사이즈" />
+      <FilterButton text="가격대" />
+      <FilterButton text="종류" />
+    </ButtonWrap>
   );
 }
+
+const ButtonWrap = styled.div`
+  display: inline-flex;
+  align-items: center;
+  gap: 13px;
+
+  width: 370px;
+  height: 33px;
+
+  margin-left: 153px;
+  margin-top: 22px;
+`;
